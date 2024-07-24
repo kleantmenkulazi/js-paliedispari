cf33d9811b23e6460270860288e2c9f9c1650c92
kleantmenkulazi <kleantmenkulazi@gmail.com>
2024-07-24 09:15:59 +0200

    Ottimizzazione js Palindromo

--- Palindromo/js/script.js
+++ Palindromo/js/script.js
@@ -5,6 +5,7 @@ const result = checkPalindrome(userWord)
 console.log('result', result, typeof result)
 if (result) {
     alert('La parola è palindroma!');
+    console.log('La parola è palindroma!')
 }
 else {
     alert('La parola Non è palindroma...');
@@ -13,6 +14,15 @@ else {
 
 
 function checkPalindrome(word) {
+    const reverseWord = reverseWord(word);
+    if (word == reverseWord) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+function reverseWord(wordTobeReversed) {
     let reverseWord = '';
     for(let i =word.leght - 1; i >= 0; i--) {
         console.log(word[i]);
@@ -20,11 +30,5 @@ function checkPalindrome(word) {
         reverseWord = reverseWord + word[i];
     }
     console.log('reverseWord', reverseWord, reverseWord.length, typeof reverseWord);
-
-    if (word == reverseWord) {
-        return true;
-    }
-    else {
-        return false;
-    }
+    return reverseWord;
 }
